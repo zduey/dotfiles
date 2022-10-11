7864251e92ce54cea3a10292f58ec93547756e37
Zach Duey <zachduey@gmail.com>
2022-10-10 23:51:17 -0400

    add key map to pull up neogit

--- .config/nvim/lua/config/neogit.lua
+++ .config/nvim/lua/config/neogit.lua
@@ -4,6 +4,12 @@ function M.setup()
   if not status_ok then
     return
   end
+  vim.api.nvim_set_keymap(
+    "n",
+    "<leader>g",
+    "<CMD>Neogit<CR>",
+    { silent = true, noremap = true }
+  )
   neogit.setup {
     integrations = {
       diffview = true
