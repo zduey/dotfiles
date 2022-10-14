db7b748da583924587f8a413f95def2a7011e3c5
Zach Duey <zachduey@gmail.com>
2022-10-14 12:43:18 -0400

    nvim-tree config

---
+++ .config/nvim/lua/config/nvimtree.lua
@@ -0,0 +1,32 @@
+local M = {}
+
+function M.setup()
+  -- Close NVIM if NvimTree is the last buffer
+  vim.cmd(
+    [[autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif]]
+  )
+  vim.api.nvim_set_keymap(
+    "n",
+    "<leader>n",
+    "<CMD>NvimTreeToggle<CR>",
+    { silent = true, noremap = true }
+  )
+  require('nvim-tree').setup({
+    disable_netrw = true,
+    hijack_netrw = true,
+    update_cwd = true,
+    update_focused_file = {
+      enable = true,
+      update_cwd = true,
+    },
+    view = {
+      number = true,
+      relativenumber = true,
+    },
+    filters = {
+      custom = { ".git" },
+    },
+  })
+  vim.g.nvim_tree_respect_buf_cwd = 1
+end
+return M
