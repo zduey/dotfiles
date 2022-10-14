a3dad6743ed66625758dba51df389ae2d74adb74
Zach Duey <zachduey@gmail.com>
2022-10-13 20:50:10 -0400

    remove keymaps and deafults

---
+++ .config/nvim/after/plugin/defaults.lua
@@ -0,0 +1,64 @@
+local api = vim.api
+local g = vim.g
+local opt = vim.opt
+
+-- Remap leader and local leader to <Space>
+api.nvim_set_keymap("", "<Space>", "<Nop>", { noremap = true, silent = true })
+g.mapleader = " "
+g.maplocalleader = " "
+
+-- General options
+opt.termguicolors = true -- Enable colors in terminal
+opt.hlsearch = true --Set highlight on search
+opt.number = true --Make line numbers default
+opt.relativenumber = true --Make relative number default
+opt.mouse = "a" --Enable mouse mode
+opt.breakindent = true --Enable break indent
+opt.undofile = true --Save undo history
+opt.ignorecase = true --Case insensitive searching unless /C or capital in search
+opt.smartcase = true -- Smart case
+opt.updatetime = 250 --Decrease update time
+opt.timeoutlen = 300 -- time to wait for mapped sequence to complete
+opt.signcolumn = "yes" -- Always show sign column
+opt.clipboard = "unnamedplus" -- Access system clipboard
+opt.backspace = "indent,eol,start",
+opt.breakindent = true,
+opt.completeopt = "menuone,noselect,menu",
+opt.concealcursor = "n",
+opt.conceallevel = 2,
+opt.cursorlineopt = "number",
+opt.diffopt = "internal,filler,closeoff,algorithm:patience",
+opt.expandtab = true,
+opt.fillchars = "fold: ,foldclose:,foldopen:,foldsep: ,diff: ,eob: ",
+opt.fixendofline = false,
+opt.foldexpr = "nvim_treesitter#foldexpr()",
+opt.foldlevel = 99,
+opt.foldmethod = "expr",
+opt.formatoptions = "lnjqr",
+opt.guicursor = "",
+opt.inccommand = "split",
+opt.laststatus = 3,
+opt.linebreak = true,
+opt.mouse = "a",
+opt.scrolloff = 10,
+opt.shiftwidth = 0,
+opt.showmode = false,
+opt.smartcase = true,
+opt.spelloptions = "noplainbuffer",
+opt.splitbelow = true,
+opt.splitright = true,
+opt.switchbuf = "useopen",
+opt.tabstop = 2,
+opt.textwidth = 120,
+opt.undodir = vim.fn.expand("~/.cache/nvim/undodir"),
+opt.undofile = true,
+opt.viewoptions = "cursor,folds,slash,unix",
+
+
+-- Highlight on yank
+vim.cmd [[
+opt.augroup YankHighlight
+opt.opt.autocmd!
+opt.opt.autocmd TextYankPost * silent! lua vim.highlight.on_yank()
+opt.augroup end
+]]
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,61 +1,9 @@
 -- global
-vim.g.mapleader = " "
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- commands
 vim.cmd([[filetype plugin indent on]])
-
--- settings
-local function set(opt, val)
-  vim.opt[opt] = val
-end
-
-for opt, val in pairs({
-  backspace = "indent,eol,start",
-  breakindent = true,
-  completeopt = "menuone,noselect,menu",
-  concealcursor = "n",
-  conceallevel = 2,
-  cursorlineopt = "number",
-  diffopt = "internal,filler,closeoff,algorithm:patience",
-  expandtab = true,
-  fillchars = "fold: ,foldclose:,foldopen:,foldsep: ,diff: ,eob: ",
-  fixendofline = false,
-  foldexpr = "nvim_treesitter#foldexpr()",
-  foldlevel = 99,
-  foldmethod = "expr",
-  formatoptions = "lnjqr",
-  guicursor = "",
-  ignorecase = true,
-  inccommand = "split",
-  laststatus = 3,
-  linebreak = true,
-  mouse = "a",
-  number = true,
-  relativenumber = true,
-  scrolloff = 10,
-  shiftwidth = 0,
-  showmode = false,
-  signcolumn = "yes",
-  smartcase = true,
-  spelloptions = "noplainbuffer",
-  splitbelow = true,
-  splitright = true,
-  switchbuf = "useopen",
-  tabstop = 2,
-  termguicolors = true,
-  textwidth = 120,
-  undodir = vim.fn.expand("~/.cache/nvim/undodir"),
-  undofile = true,
-  viewoptions = "cursor,folds,slash,unix",
-  wrap = false,
-}) do
-  local suc, err = pcall(set, opt, val)
-  if not suc then
-    print("Error setting option " .. opt .. ": " .. err)
-  end
-end
 
 -- Set up plugins
 require("plugins")
