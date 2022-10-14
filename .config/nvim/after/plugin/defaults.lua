57169774e95cc82e9b27eafeb0ef219ce1e5758c
Zach Duey <zachduey@gmail.com>
2022-10-13 22:46:49 -0400

    fix options settings

--- .config/nvim/after/plugin/defaults.lua
+++ .config/nvim/after/plugin/defaults.lua
@@ -21,44 +21,44 @@ opt.updatetime = 250 --Decrease update time
 opt.timeoutlen = 300 -- time to wait for mapped sequence to complete
 opt.signcolumn = "yes" -- Always show sign column
 opt.clipboard = "unnamedplus" -- Access system clipboard
-opt.backspace = "indent,eol,start",
-opt.breakindent = true,
-opt.completeopt = "menuone,noselect,menu",
-opt.concealcursor = "n",
-opt.conceallevel = 2,
-opt.cursorlineopt = "number",
-opt.diffopt = "internal,filler,closeoff,algorithm:patience",
-opt.expandtab = true,
-opt.fillchars = "fold: ,foldclose:,foldopen:,foldsep: ,diff: ,eob: ",
-opt.fixendofline = false,
-opt.foldexpr = "nvim_treesitter#foldexpr()",
-opt.foldlevel = 99,
-opt.foldmethod = "expr",
-opt.formatoptions = "lnjqr",
-opt.guicursor = "",
-opt.inccommand = "split",
-opt.laststatus = 3,
-opt.linebreak = true,
-opt.mouse = "a",
-opt.scrolloff = 10,
-opt.shiftwidth = 0,
-opt.showmode = false,
-opt.smartcase = true,
-opt.spelloptions = "noplainbuffer",
-opt.splitbelow = true,
-opt.splitright = true,
-opt.switchbuf = "useopen",
-opt.tabstop = 2,
-opt.textwidth = 120,
-opt.undodir = vim.fn.expand("~/.cache/nvim/undodir"),
-opt.undofile = true,
-opt.viewoptions = "cursor,folds,slash,unix",
-
+opt.backspace = "indent,eol,start"
+opt.breakindent = true
+opt.completeopt = "menuone,noselect,menu"
+opt.concealcursor = "n"
+opt.conceallevel = 2
+opt.cursorlineopt = "number"
+opt.diffopt = "internal,filler,closeoff,algorithm:patience"
+opt.expandtab = true
+opt.fillchars = "fold: ,foldclose:,foldopen:,foldsep: ,diff: ,eob: "
+opt.fixendofline = false
+opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.foldlevel = 99
+opt.foldmethod = "expr"
+opt.formatoptions = "lnjqr"
+opt.guicursor = ""
+opt.inccommand = "split"
+opt.laststatus = 3
+opt.linebreak = true
+opt.mouse = "a"
+opt.scrolloff = 10
+opt.shiftwidth = 0
+opt.showmode = false
+opt.smartcase = true
+opt.spelloptions = "noplainbuffer"
+opt.splitbelow = true
+opt.splitright = true
+opt.switchbuf = "useopen"
+opt.tabstop = 2
+opt.textwidth = 120
+opt.undodir = vim.fn.expand("~/.cache/nvim/undodir")
+opt.undofile = true
+opt.viewoptions = "cursor,folds,slash,unix"
 
 -- Highlight on yank
 vim.cmd [[
-opt.augroup YankHighlight
-opt.opt.autocmd!
-opt.opt.autocmd TextYankPost * silent! lua vim.highlight.on_yank()
-opt.augroup end
+  augroup YankHighlight
+    autocmd!
+    autocmd TextYankPost * silent! lua vim.highlight.on_yank()
+  augroup end
 ]]
+
