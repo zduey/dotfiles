06766eb815ca8c30cb4b6dc0afaf6a2390e23ea0
Zach Duey <zduey@activatecare.com>
2022-10-31 09:39:52 -0400

    add rbenv and pyenv for ruby and python dev

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,8 @@
 # enable starship prompt
 starship init fish | source
+
+# Load rbenv automatically
+status --is-interactive; and rbenv init - fish | source
+
+# Load pyenv automatically
+pyenv init - | source
